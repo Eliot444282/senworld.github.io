eaa5586b8bf326c59df45a57139ac56f72b32dbb
Eliot444282 <axolonn@gmail.com>
2025-10-22 17:54:29 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXX",
+  authDomain: "senworld-io.firebaseapp.com",
+  projectId: "senworld-io",
+  storageBucket: "senworld-io.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefg123456"
+};
