7b4f9afa0a5649f6cedd7e846777c212474e26b8
Eliot444282 <axolonn@gmail.com>
2025-10-22 16:43:09 +0300

    Update game.js

--- game.js
+++ game.js
@@ -7,25 +7,25 @@ canvas.height = window.innerHeight;
 let player = {
   x: canvas.width / 2,
   y: canvas.height / 2,
-  size: 30,
+  size: 25,
   color: "green",
   speed: 5
 };
 
-let food = [];
+let foods = [];
 
-function spawnFood() {
-  for (let i = 0; i < 15; i++) {
-    food.push({
+function createFood() {
+  for (let i = 0; i < 20; i++) {
+    foods.push({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
-      size: 10,
+      size: 8,
       color: "red"
     });
   }
 }
 
-spawnFood();
+createFood();
 
 window.addEventListener("mousemove", (e) => {
   player.x = e.clientX;
@@ -39,39 +39,40 @@ function drawPlayer() {
   ctx.fill();
 }
 
-function drawFood() {
-  for (let i = 0; i < food.length; i++) {
+function drawFoods() {
+  for (let f of foods) {
     ctx.beginPath();
-    ctx.arc(food[i].x, food[i].y, food[i].size, 0, Math.PI * 2);
-    ctx.fillStyle = food[i].color;
+    ctx.arc(f.x, f.y, f.size, 0, Math.PI * 2);
+    ctx.fillStyle = f.color;
     ctx.fill();
   }
 }
 
 function checkEat() {
-  for (let i = 0; i < food.length; i++) {
-    let dx = player.x - food[i].x;
-    let dy = player.y - food[i].y;
-    let dist = Math.sqrt(dx * dx + dy * dy);
-    if (dist < player.size + food[i].size) {
+  for (let i = foods.length - 1; i >= 0; i--) {
+    const f = foods[i];
+    const dx = player.x - f.x;
+    const dy = player.y - f.y;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < player.size + f.size) {
+      foods.splice(i, 1);
       player.size += 1;
-      food.splice(i, 1);
-      food.push({
+      foods.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-        size: 10,
+        size: 8,
         color: "red"
       });
     }
   }
 }
 
-function animate() {
+function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawFood();
+  drawFoods();
   drawPlayer();
   checkEat();
-  requestAnimationFrame(animate);
+  requestAnimationFrame(loop);
 }
 
-animate();
+loop();
